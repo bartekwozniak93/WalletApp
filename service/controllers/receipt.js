75115db71a963d74b89e8827036a46517461fbb4
qba08 <jakubh08@gmail.com>
2016-06-11 20:33:10 +0200

    Update receipt.js

--- service/controllers/receipt.js
+++ service/controllers/receipt.js
@@ -1,70 +1,149 @@
-var auth = require('google-api-utility')		
-, autrequest = auth.request		
-
-exports.OCR = function(imgContent, callback){		
-	try{		
-		var jsonfile = 'service/MY_KEY.json';		
-  		var baseurl = 'https://vision.googleapis.com/v1/images:annotate';		
-  		auth.init({		
-  			scope: ['https://www.googleapis.com/auth/cloud-platform'].join(' '),		
-  			json_file: jsonfile		
-  		});		
-  		
-  		req ={		
-  			image: {content: imgContent.split(',')[1]},		
-          		features:[],		
-  		}		
-  		req.features.push({ 		
-  			type:'TEXT_DETECTION',		
-  			maxResults: 1		
-  			});		
-  		request={		
-  			requests:[],		
-  		}		
-  		request.requests.push(req);			
-  		autrequest(		
-  			{url:baseurl,		
-  			method:'post',		
-  			json:request},		
-  			function (error, req, dat){		
-  				if(error)callback(error,null);		
- 				else{		
- 					data = JSON.stringify(dat.responses[0].textAnnotations[0].description); 					
- 					lines=data.split('\\n');		
-					allText=lines.join(' ');		
- 				//Price		
- 					try{		
- 					if(a = allText.match(new RegExp('(S|s)(U|u)(M|m|N|n)(A|a)( )*(.){2,3}( )* [0-9]*(,)[0-9]{2}', 'g'))) sPrice=parseFloat(a[0].split(' ')[2].replace(",","."));		
- 					else if (a = allText.match(new RegExp('(S|s)(U|u)(M|m|N|n)(A|a)(:)( )*[0-9]*(,)[0-9]{2}', 'g'))) sPrice=parseFloat(a[0].split(' ')[1].replace(",","."));		
- 					else if (a = allText.match(new RegExp('(S|s)(U|u)(M|m|N|n)(A|a)( )*[0-9]*(,)[0-9]{2}', 'g'))) sPrice=parseFloat(a[0].split(' ')[1].replace(",","."));		
- 					else if (a = allText.match(new RegExp('(R|r)(A|a)(Z|z)(E|e)(M|m)( )*(.){2,3}( )* [0-9]*(,)[0-9]{2}', 'g'))) sPrice=parseFloat(a[0].split(' ')[2].replace(",","."));		
- 					else if (a = allText.match(new RegExp('(P|p)(L|l)(N|n)( )*[0-9]*(,)[0-9]{2}', 'g'))) sPrice=parseFloat(a[0].split(' ')[1].replace(",","."));		
- 					else sPrice=" ";		
- 					}catch(err){ sPrice=" ";}		
- 				//Time		
- 					if(a = allText.match(new RegExp('[0-9]{4}(-)[0-9]{2}(-)[0-9]{2}', 'g'))) sData=a[0];		
- 					else sData=" ";		
- 				//NIP		
- 					if(a = allText.match(new RegExp('[0-9]{2,4}(-)[0-9]{2,4}(-)[0-9]{2,4}(-)[0-9]{2,4}', 'g'))) sNIP=a[0];		
-					else sNIP=" ";		
- 				//Name		
- 					try{		
- 						sName=lines[0].replace("\"", "");		
- 					}catch(err){sName=" ";}		
- 								
- 					out={		
- 						textReceipt:allText,		
- 						companyName:sName,			
- 						nip:sNIP,		
- 						dateReceipt:sData,		
- 						price:sPrice		
- 					};		
- 					callback(null,out);		
- 				}		
- 			}		
- 				
- 		);		
- 	}catch(err){		
- 		callback(err,null);		
- 	}		
- }
+ var Receipt = require('../models/receipt');
+  var ocr=require('../models/ocr').OCR;
+  var ctn=require('../models/imageProcessing').CreateTN;
+  
+  exports.postAtt = function(req, res) {
+      try{
+          ocr(req.body.att,function(err,data){
+              if(err) throw err;
+              var receipt = new Receipt();
+              receipt.att = req.body.att;
+              receipt.textReceipt = data.textReceipt;
+              receipt.userId = req.user._id;
+  
+              if(req.body.dateCreation == undefined)
+                  receipt.dateCreation = Date.now();
+              else
+                  receipt.dateCreation = req.body.dateCreation;
+  
+              if(req.body.companyName == undefined)
+                  receipt.companyName = data.companyName;
+              else
+                  receipt.companyName = req.body.companyName;
+  
+              if(req.body.nip == undefined)
+                  receipt.nip = data.nip;
+              else
+                  receipt.nip = req.body.nip;
+  
+              if(req.body.dateReceipt == undefined)
+                  receipt.dateReceipt = data.dateReceipt;
+              else
+                  receipt.dateReceipt = req.body.dateReceipt;
+  
+              if(req.body.price == undefined)
+                  receipt.price = data.price;
+              else
+                  receipt.price = req.body.receipt;
+              ctn(req.body.att,function(err,data){
+                  if(err) receipt.attTN="";
+                  else receipt.attTN=data;
+                  receipt.save(function(err) {
+                      if (err)
+                          res.send(err);
+                      receipt.att = "";
+                      res.json({ message: 'Receipt added to the wallet!', id:receipt._id, data: receipt });
+                  });
+              });
+              
+          });
+  	}catch(err){
+  		res.json({message: 'Unable to read att ', error: err});
+  	}
+  };
+  
+  /*exports.postReceipts = function(req, res) {
+      var receipt = new Receipt();
+      receipt.userId = req.user._id;
+      receipt.categoryId = req.body.categoryId;
+      receipt.dateCreation = Date.now();
+      receipt.dateReceipt = req.body.dateReceipt;
+      receipt.companyName = req.body.companyName;
+      receipt.nip = req.body.nip;
+      receipt.price = req.body.price;
+      receipt.textReceipt = "";
+      receipt.att = req.body.att;
+  
+      receipt.save(function(err) {
+          if (err)
+              res.send(err);
+  
+          res.json({ message: 'Receipt added to the wallet!', id:receipt._id, data: receipt });
+      });
+  }*/
+  
+  exports.getReceipts = function(req, res) {
+      Receipt.find({userId:req.user._id},function(err, receipts) {
+          if (err)
+              res.send(err);
+          allReceipts=[];
+          for(var num in receipts)
+              receipts[num].att = "";
+              allReceipts.push({id:receipts[num], data:receipts[num]});
+          res.json(allReceipts);
+     });
+  };
+  
+  exports.getReceipt = function(req, res) {
+      Receipt.findById(req.params.receipt_id, function(err, receipt) {
+          if (err)
+              res.send(err);
+          if (!receipt || req.user._id !=  receipt.userId)
+              res.json({message: 'Could not find receipt in the wallet!', data:null});
+          else{
+              receipt.att = "";
+              res.json({message: 'Receipt found in the wallet!', data:receipt});
+          }
+      });
+  };
+  
+  exports.getReceiptAtt = function(req, res) {
+      Receipt.findById(req.params.receipt_id, function(err, receipt) {
+          if (err)
+              res.send(err);
+          if (!receipt || req.user._id !=  receipt.userId)
+              res.json({message: 'Could not find receipt in the wallet!', data:null});
+          else{
+              res.json({message: 'Receipt found in the wallet!', att:receipt.att});
+          }
+      });
+  };
+  
+  exports.putReceipt = function(req, res) {
+      Receipt.findById(req.params.receipt_id, function(err, receipt) {
+          if (err)
+              res.send(err);
+          if (!receipt || req.user._id !=  receipt.userId)
+              res.json({message: 'Could not find receipt in the wallet!', data:null});
+          else{
+              receipt.categoryId = req.body.categoryId;
+              receipt.LastModification = Date.now();
+              receipt.dateReceipt = req.body.dateReceipt;
+              receipt.companyName = req.body.companyName;
+              receipt.nip = req.body.nip;
+              receipt.price = req.body.price;
+  
+              receipt.save(function(err) {
+                  if (err)
+                      res.send(err);
+                  receipt.att = "";
+                  res.json({ message: 'Receipt update in the wallet!', data: receipt });
+              });
+          }
+      });
+  };
+  
+  exports.deleteReceipt = function(req, res) {
+      Receipt.findById(req.params.receipt_id, function(err, receipt) {
+          if (err)
+              res.send(err);
+          if (!receipt || req.user._id !=  receipt.userId)
+              res.json({message: 'Could not find receipt in the wallet!'});
+          else{
+              receipt.remove(function(err) {
+                  if (err) res.send(err);
+                  res.json({message: 'Receipt successfully deleted!'});
+              });
+  	   }
+      });
+  }
